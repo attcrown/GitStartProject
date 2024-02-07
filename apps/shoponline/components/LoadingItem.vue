139a22f1328f218f7a297a8bf1891483bf368d45
Mr.Vongvaris Chawawiwat <62502710+attcrown@users.noreply.github.com>
2024-02-07 19:22:30 +0700

    Setup vueShop

---
+++ apps/shoponline/components/LoadingItem.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-skeleton-loader class="mx-auto" max-width="300" type="card"></v-skeleton-loader>
+</template>
+  
+<script>
+export default {
+    inject: {
+        theme: {
+            default: { isDark: false },
+        },
+    },
+}
+</script>
---
+++ apps/shoponline/layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-app> 
+        <desktopDefault v-if="!deviceMode"></desktopDefault>
+        <mobileDefault v-if="deviceMode"></mobileDefault>  
+        <v-main :class="!deviceMode ? 'mt-11' : 'mt-0'">
+            <Nuxt />
+        </v-main>        
+    </v-app>
+</template>
+<script>
+import desktopDefault from './desktopDefault.vue';
+import mobileDefault from './mobileDefault.vue';
+export default {
+    name: "defalutLayout",
+    data() {
+        return {
+            deviceMode: false,
+        };
+    },
+    components: {
+        desktopDefault,
+        mobileDefault
+    },
+    mounted() {
+        console.log(this.$store.state);       
+        this.checkMobile();
+        window.addEventListener('resize', this.checkMobile);
+    },
+    methods: {        
+        checkMobile() {
+            this.deviceMode = window.innerWidth <= 768; // ตั้งค่าให้เป็น mobile ถ้าขนาดน้อยกว่าหรือเท่ากับ 768 pixels
+            this.$store.commit("SET_DEVICE", this.deviceMode);
+        },
+    },
+};
+</script>
