4916ef1f48295a4021fae3fb3409ab9111ec8770
Mr.Vongvaris Chawawiwat <62502710+attcrown@users.noreply.github.com>
2024-02-11 16:29:19 +0700

    edit file ts

--- apps/api-exc-shop/src/index.ts
+++ apps/api-exc-shop/src/index.ts
@@ -1,11 +1,12 @@
 import { appfirebase, db } from './services/config';
-import express from "express";
+import express, { json } from "express";
 const app = express();
 const port = 3001;
 
 app.get("/", (req, res) => {
   try {
     res.send({ result: "Hello World!", db });
+    console.log('success DB')
   } catch (error) {
     console.error("Error accessing Firebase database:", error);
     res.status(500).send({ error: "Internal Server Error" });
